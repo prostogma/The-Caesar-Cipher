731c87b820ac230c36927a6399e2b4b6e72236b1
prostogma <example@gmail.com>
2025-04-10 14:23:53 +0300

    add-encryption-description

--- WindowsForms/WindowsForms/Form1.Designer.cs
+++ WindowsForms/WindowsForms/Form1.Designer.cs
@@ -28,13 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.LanguageChoice = new System.Windows.Forms.ComboBox();
+            this.numericUpDownShift = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SelectEncDec = new System.Windows.Forms.ComboBox();
+            this.buttonLaunch = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownShift)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(355, 45);
+            this.textBoxInput.MaxLength = 1500;
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxInput.Size = new System.Drawing.Size(580, 106);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // LanguageChoice
+            // 
+            this.LanguageChoice.FormattingEnabled = true;
+            this.LanguageChoice.Items.AddRange(new object[] {
+            "Русский",
+            "Английский"});
+            this.LanguageChoice.Location = new System.Drawing.Point(532, 202);
+            this.LanguageChoice.Name = "LanguageChoice";
+            this.LanguageChoice.Size = new System.Drawing.Size(190, 21);
+            this.LanguageChoice.TabIndex = 1;
+            // 
+            // numericUpDownShift
+            // 
+            this.numericUpDownShift.Location = new System.Drawing.Point(532, 262);
+            this.numericUpDownShift.Maximum = new decimal(new int[] {
+            33,
+            0,
+            0,
+            0});
+            this.numericUpDownShift.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownShift.Name = "numericUpDownShift";
+            this.numericUpDownShift.Size = new System.Drawing.Size(190, 20);
+            this.numericUpDownShift.TabIndex = 2;
+            this.numericUpDownShift.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(528, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выбор языка";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(528, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Диапазон шифрования";
+            // 
+            // SelectEncDec
+            // 
+            this.SelectEncDec.FormattingEnabled = true;
+            this.SelectEncDec.Items.AddRange(new object[] {
+            "Шифровать",
+            "Дешифровать"});
+            this.SelectEncDec.Location = new System.Drawing.Point(532, 316);
+            this.SelectEncDec.Name = "SelectEncDec";
+            this.SelectEncDec.Size = new System.Drawing.Size(190, 21);
+            this.SelectEncDec.TabIndex = 5;
+            // 
+            // buttonLaunch
+            // 
+            this.buttonLaunch.Location = new System.Drawing.Point(487, 376);
+            this.buttonLaunch.Name = "buttonLaunch";
+            this.buttonLaunch.Size = new System.Drawing.Size(120, 27);
+            this.buttonLaunch.TabIndex = 6;
+            this.buttonLaunch.Text = "Выполнить";
+            this.buttonLaunch.UseVisualStyleBackColor = true;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(355, 462);
+            this.textBoxOutput.MaxLength = 1500;
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxOutput.Size = new System.Drawing.Size(580, 100);
+            this.textBoxOutput.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(528, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Шифровать/Дешифровать";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1269, 683);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonLaunch);
+            this.Controls.Add(this.SelectEncDec);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownShift);
+            this.Controls.Add(this.LanguageChoice);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownShift)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.ComboBox LanguageChoice;
+        private System.Windows.Forms.NumericUpDown numericUpDownShift;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox SelectEncDec;
+        private System.Windows.Forms.Button buttonLaunch;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsForms/WindowsForms/Form1.cs
+++ WindowsForms/WindowsForms/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsForms
@@ -15,6 +8,119 @@ namespace WindowsForms
         public Form1()
         {
             InitializeComponent();
+
+            // Настройка ComboBox для выбора языка (без дублирования)
+            LanguageChoice.Items.Clear(); // Очищаем, если были дубли
+            LanguageChoice.Items.Add("Русский");
+            LanguageChoice.Items.Add("Английский");
+            LanguageChoice.SelectedIndex = 0; // По умолчанию русский
+
+            // Настройка ComboBox для выбора действия
+            SelectEncDec.Items.Clear();
+            SelectEncDec.Items.Add("Шифровать");
+            SelectEncDec.Items.Add("Дешифровать");
+            SelectEncDec.SelectedIndex = 0; // По умолчанию шифрование
+
+            // Настройка NumericUpDown для сдвига
+            numericUpDownShift.Minimum = 1;
+            numericUpDownShift.Maximum = 33; // Для русского (по умолчанию)
+            numericUpDownShift.Value = 3;    // Классический сдвиг Цезаря
+
+            // Обработчик изменения языка
+            LanguageChoice.SelectedIndexChanged += (sender, e) =>
+            {
+                // Устанавливаем максимальный сдвиг в зависимости от языка
+                numericUpDownShift.Maximum = (LanguageChoice.SelectedIndex == 0) ? 33 : 26;
+
+                // Если текущее значение сдвига больше нового максимума, исправляем
+                if (numericUpDownShift.Value > numericUpDownShift.Maximum)
+                {
+                    numericUpDownShift.Value = numericUpDownShift.Maximum;
+                }
+            };
+
+            // Обработчик кнопки "Выполнить"
+            buttonLaunch.Click += ButtonLaunch_Click;
+        }
+
+        private void ButtonLaunch_Click(object sender, EventArgs e)
+        {
+            // Получаем текст из поля ввода
+            string text = textBoxInput.Text;
+
+            // Проверка на пустой ввод
+            if (string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("Введите текст!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Проверка, что язык выбран (SelectedIndex = -1 означает отсутствие выбора)
+            if (LanguageChoice.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите язык!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Получаем значение сдвига из NumericUpDown
+            int shift = (int)numericUpDownShift.Value;
+
+            // Определяем язык: 0 - Русский, 1 - Английский
+            bool isRussian = (LanguageChoice.SelectedIndex == 0);
+
+            // Определяем режим: 0 - Шифровать, 1 - Дешифровать
+            bool isEncrypt = (SelectEncDec.SelectedIndex == 0);
+
+            // Вызываем функцию шифрования/дешифрования
+            string result = CaesarCipher(text, shift, isRussian, isEncrypt);
+
+            // Выводим результат в поле вывода
+            textBoxOutput.Text = result;
+        }
+
+        private string CaesarCipher(string text, int shift, bool isRussian, bool isEncrypt)
+        {
+            // Выбираем алфавит в зависимости от языка
+            string alphabet = isRussian ?
+                "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" : // Русский алфавит
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ";         // Английский алфавит
+
+            // Создаем полный алфавит: строчные + заглавные буквы для регистронезависимой обработки
+            alphabet = alphabet.ToLower() + alphabet;
+
+            // Если режим дешифровки, инвертируем сдвиг
+            shift = isEncrypt ? shift : -shift;
+
+            // Создаем массив для хранения результата
+            char[] result = new char[text.Length];
+
+            // Обрабатываем каждый символ входного текста
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                // Ищем индекс символа в алфавите (в верхнем регистре)
+                int index = alphabet.IndexOf(char.ToUpper(c));
+
+                if (index >= 0)
+                {
+                    // Вычисляем новый индекс с учетом сдвига
+                    int newIndex = (index + shift) % alphabet.Length;
+
+                    // Корректируем индекс, если он отрицательный (для дешифровки)
+                    if (newIndex < 0) newIndex += alphabet.Length;
+
+                    // Сохраняем символ, учитывая исходный регистр
+                    result[i] = char.IsLower(c) ?
+                        char.ToLower(alphabet[newIndex]) :  // Сохраняем строчную букву
+                        alphabet[newIndex];                 // Сохраняем заглавную букву
+                }
+                else
+                {
+                    result[i] = c;
+                }
+            }
+
+            return new string(result);
         }
     }
 }
